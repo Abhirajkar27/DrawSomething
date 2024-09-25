b3d19f1a03638b202f8545b9163fbeff60407339
Abhirajkar Bajpai <abhirajkar.bajpai@vyld.io>
2024-09-25 15:43:19 +0530

    first page Completed

--- src/components/WordSelection.jsx
+++ src/components/WordSelection.jsx
@@ -13,20 +13,38 @@ const WordSelection = (props) => {
     useContext(GameContext);
 
   const [isPaintVisible, setIsPaintVisible] = useState(false);
-  const [options, setOptions]=useState([]);
+  const [options, setOptions] = useState([]);
 
   const handleWordSelect = (word) => {
     console.log("here", word);
     setSelectedWord(word);
   };
 
+  const selectRandomWords = () => {
+    const shuffledWords = words.sort(() => Math.random() - 0.5);
+    return shuffledWords.slice(0, 3);
+  };
+
   useEffect(() => {
-    const selectRandomWords = () => {
-      const shuffledWords = words.sort(() => Math.random() - 0.5); 
-      return shuffledWords.slice(0, 3); 
-    };
     setOptions(selectRandomWords());
   }, []);
+
+  const getNewOptions = () => {
+    console.log("hey");
+    const optionsSet = new Set(options);
+    const newOptions = [];
+    const shuffledWords = words.sort(() => Math.random() - 0.5);
+    for (const word of shuffledWords) {
+      if (!optionsSet.has(word)) {
+        newOptions.push(word);
+      }
+      if (newOptions.length === 3) {
+        setOptions(newOptions);
+        break;
+      }
+    }
+    return;
+  };
 
   const handleStart = () => {
     if (selectedWord) {
@@ -41,7 +59,7 @@ const WordSelection = (props) => {
     if (isChallenge) {
       props.setAppPage("ChallengeMode");
     }
-  },[]);
+  }, []);
 
   if (isPaintVisible) {
     return <Paint selectedWord={selectedWord} />;
@@ -61,18 +79,31 @@ const WordSelection = (props) => {
         <img className="box_G6h5" src={picking_contn} alt="container_box"></img>
         <div>
           <span>Choose a word to Draw</span>
-          <div style={{marginTop:'4vh'}}>
-          {options.map((word, index) => (
-            <div key={index} className="word-image-wrapper" onClick={() => handleWordSelect(word)}>
-              <img src={option_contn} alt={word} className="word-image" />
-              <div className="word-overlay">{word}</div>
-            </div>
-          ))}
+          <div style={{ marginTop: "4vh" }}>
+            {options.map((word, index) => (
+              <div
+                key={index}
+                className="word-image-wrapper"
+                onClick={() => handleWordSelect(word)}
+              >
+                <img src={option_contn} alt={word} className="word-image" />
+                <div className="word-overlay">{word}</div>
+              </div>
+            ))}
           </div>
-          <img className="new_option_btn_G6h5" src={new_btn}/>
+          <img
+            onClick={getNewOptions}
+            className="new_option_btn_G6h5"
+            src={new_btn}
+          />
         </div>
       </div>
-        <img onClick={handleStart} className="startbtn_G6h5" src={submit_btn} alt="submit_btn"/>
+      <img
+        onClick={handleStart}
+        className="startbtn_G6h5"
+        src={submit_btn}
+        alt="submit_btn"
+      />
     </div>
   );
 };
